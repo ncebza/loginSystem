64567127b57c985b23be431e16d6671c10b8fd8a
Student <Student@LabVM2049939.yyy1nizreh5ejd0ikvvqoucftf.jnbx.internal.cloudapp.net>
2026-09-16 21:44:23 +0200

    Initial commit

---
+++ src/test/java/LoginClassTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Student
+ */
+import com.mycompany.loginsystem_nceba_part1.LoginClass;
+import org.junit.jupiter.api.Test;
+import static
+org.junit.jupiter.api.Assertions.*;
+ss
+public class LoginClassTest {
+   @Test
+   public void testLoginSuccess(){
+       LoginClass login = new LoginClass();
+       boolean result = login.loginUser("user_1", "WrongPass", "user_1", "Pass123!" );
+       assertFalse(result);
+   }
+}
